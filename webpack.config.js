2b1aef54a0a34285b50521976d3ef0539fa225e9
Muridi010 <mohamudsharif017@gmail.com>
2020-07-20 17:03:24 +0300

    Set up devDependancies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,9 @@ module.exports = (env) => {
   const isProduction = env == 'production';
   const CSSExtract = new ExtractTextPlugin('styles.css');
   return {
-    mode: 'development',
     entry: './src/index.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js',
     },
     module: {
@@ -45,6 +44,7 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
+      publicPath: '/dist/',
     },
   };
 };
